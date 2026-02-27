3989be955aefccfe95245a7fa113a3ad9d089643
Oleksandr Tretiak <o.s.tretial@gmail.com>
2026-02-27 23:20:11 +0200

    Fix Tailwind styles import

--- app/layout.js
+++ app/layout.js
@@ -1,16 +1,16 @@
+import "./globals.css";
+
 export const metadata = {
   title: "Єдина цифрова платформа потреб громад",
   description:
     "Єдиний електронний майданчик для прозорого розміщення потреб громад та ефективного залучення донорської й інвестиційної підтримки.",
-  icons: {
-    icon: "/favicon.ico",
-  },
+  icons: { icon: "/favicon.ico" },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="uk">
-      <body>{children}</body>
+      <body className="antialiased">{children}</body>
     </html>
   );
 }
